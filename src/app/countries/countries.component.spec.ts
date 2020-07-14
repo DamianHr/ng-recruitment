e1a019bec6d63f2503c8d6de7cb4d7a71d5bffd3
Michał Mrozek <michalmrozek@wp.pl>
2020-07-14 15:37:46 +0200

    fix: countries test

--- src/app/countries/countries.component.spec.ts
+++ src/app/countries/countries.component.spec.ts
@@ -1,6 +1,13 @@
+import { CdkTableModule } from '@angular/cdk/table';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatInputModule } from '@angular/material/input';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { CountriesComponent } from './countries.component';
 
 describe('CountriesComponent', () => {
@@ -9,7 +16,16 @@ describe('CountriesComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule],
+      imports: [
+        HttpClientTestingModule,
+        MatToolbarModule,
+        MatInputModule,
+        MatTableModule,
+        MatSortModule,
+        CdkTableModule,
+        MatSliderModule,
+        MatPaginatorModule,
+      ],
       declarations: [CountriesComponent],
       schemas: [NO_ERRORS_SCHEMA],
     }).compileComponents();
